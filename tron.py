f40cc65b9cd81eb3ba47734407b0d2ac84adbaec
Stephen Koo <stephen.theking@gmail.com>
2018-05-14 23:14:49 -0700

    Add slack and email integrations

--- tron.py
+++ tron.py
@@ -20,9 +20,10 @@ import yaml
 from docopt import docopt
 import getpass
 import requests
+import sendgrid
+from sendgrid.helpers.mail import *
 
 WHATS_NEXT_ID = 'hRTOsDzc' # What's Next
-LIST_ID = '55b2fb01d72f8d313d8e9acc' # Today
 
 config = {}
 
@@ -59,11 +60,38 @@ def move_cards(source_list, target_list):
         r.raise_for_status()
 
 
+def send_email(config, to, subject, message):
+    if 'sendgrid' in config:
+        sg = sendgrid.SendGridAPIClient(apikey=config['sendgrid']['api_key'])
+        from_email = Email(config['sendgrid']['reply_to'])
+        to_email = Email(to)
+        content = Content("text/plain", message)
+        mail = Mail(from_email, subject, to_email, content)
+        response = sg.client.mail.send.post(request_body=mail.get())
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+
+
+def send_slack(config, message, channel="#chat"):
+    r = requests.post(config['slack']['webhook_url'], json={
+        "text": message.format(channel=channel),
+        "channel": channel,
+        "link_names": 1,
+        "username": "tron",
+        "icon_emoji": ":hamster:"
+    })
+    print(r.text)
+
+
 def main(args):
     global config
 
     with open(args['--config'], 'r') as config_file:
         config = yaml.load(config_file)
+
+    # send_slack(config, 'hello {channel}, please share the tofu', channel="@stephen")
+    # exit()
 
     if 'token' not in config:
         print('Please authorize:')
@@ -81,6 +109,7 @@ def main(args):
 
     if args['weekly']:
         move_cards(this_week, runway)
+
 
 
 if __name__ == '__main__':
